6b2948c42a118f833791b93d97791ff6bd19cf61
DongMZ1 <915094594@qq.com>
2021-03-15 23:04:09 -0400

    update main idea for this project --> useReducer and useContext for state manage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import React, {useState} from 'react'
-import LoginContext from './usecontext/logincontext'
+import React, {useState, useReducer} from 'react'
+import {LoginContext} from './usecontext/logincontext'
 import { HashRouter as Router, Switch, Route} from 'react-router-dom';
+
+
+/* The state control for login/log out is using useContext hook to pass useReducer to child component*/ 
+function loginReducer(state, action){
+  switch(action.type){
+    case 'login' :
+      return{
+        ...state,
+        isLogin: action.isLogin
+      }
+  }
+
+}
 const App = () => {
- const[loginState, setLoginState] = useState(false)
+
+ const[loginState, setLoginDispatch] = useReducer(loginReducer, {
+   isLogin: false
+ })
 
   return (
     /*Pass LoginState and SetLoginState as an object to usecontext hook for manage content showing*/ 
-    <LoginContext.Provider value ={{loginState, setLoginState}}>
+    <LoginContext.Provider value ={{loginState, setLoginDispatch}}>
+      <h1>{loginState.isLogin ? <>U are not login, click to login</> : <>U are logined, click to login out</>}</h1>
+      <button onClick={()=>setLoginDispatch({type:'login', isLogin: true})}>Click to login</button>
+      <button onClick={()=>setLoginDispatch({type:'login', isLogin: false})}>Click to log out</button>
 
 
     </LoginContext.Provider>
--- src/usecontext/logincontext.js
+++ src/usecontext/logincontext.js
@@ -1,3 +1,3 @@
-import React from 'react'
+import {createContext} from 'react'
 
-export default LoginContext = React.createContext(null);
+export const LoginContext = createContext(null);
