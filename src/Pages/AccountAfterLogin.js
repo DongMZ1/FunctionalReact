3ba1bd15c2cf602f4963556ac9d21f042c1a6290
DongMZ1 <915094594@qq.com>
2021-08-23 02:54:28 -0400

    frontend update

--- src/Pages/AccountAfterLogin.js
+++ src/Pages/AccountAfterLogin.js
@@ -3,7 +3,7 @@ import { LoginContext } from "../usecontext/logincontext";
 import { Container, Button, Row, Col } from "react-bootstrap";
 import ProductCards from "../Component/ProductCards";
 import { Link } from "react-router-dom";
-const AfterLogin = ({setshowerrorcard, seterrorcardmessage}) => {
+const AfterLogin = ({ setshowerrorcard, seterrorcardmessage }) => {
   const { loginState, setLoginDispatch } = useContext(LoginContext);
   const [showshoppingcart, setshowshoppingcart] = useState(true);
   const [showorder, setshoworder] = useState(false);
@@ -20,8 +20,8 @@ const AfterLogin = ({setshowerrorcard, seterrorcardmessage}) => {
     .toFixed(2);
 
 
-//start show product the user is currently ordering --- user has place the order --------------------------------
-  const productorderingjsx = 
+  //start show product the user is currently ordering --- user has place the order --------------------------------
+  const productorderingjsx =
     loginState.productordering.map((product) => (
       <>
         <div className="card mb-3">
@@ -49,68 +49,69 @@ const AfterLogin = ({setshowerrorcard, seterrorcardmessage}) => {
         </div>
       </>
     ))
-  
-//start show product the user is currently ordering --- user has place the order --------------------------------
+
+  //start show product the user is currently ordering --- user has place the order --------------------------------
 
   return (
-    <Row className='mx-2 mt-3 blue-color'>
-      <h6 className='width-max-content'>{loginState.email}</h6>
-    {/*start button to show view shopping cart ------------------------------------------- */}
-     <div className='disp-flex mb-3'>
-      <div
-        className={`font-14p cursor-pointer bold ${showshoppingcart && 'border-bottom-blue'} width-max-content`}
-        onClick={() => {
-          setshowshoppingcart(true);
-          setshoworder(false);
-          setshoworderhistory(false);
-        }}
-      >
-        Shopping Cart
-      </div>
-      {/*end button to show view shopping cart ------------------------------------------- */}
-      
-      {/*start button to show view current orders cart ------------------------------------------- */}   
-      <div
-        className={`font-14p cursor-pointer ml-5 bold ${showorder && 'border-bottom-blue'} width-max-content`}
-        onClick={() => {
-          setshowshoppingcart(false);
-          setshoworder(true);
-          setshoworderhistory(false);
-        }}
-      >
-        Current Orders
-      </div>
-    {/*end button to show view current orders cart ------------------------------------------- */}  
+    <Row className='mx-2 mt-3 blue-color min-height-25rem'>
+      <Col lg={6} className='offset-lg-2 offset-md-1'>
+        <h6 className='width-max-content'>{loginState.email}</h6>
+        {/*start button to show view shopping cart ------------------------------------------- */}
+        <div className='disp-flex mb-3'>
+          <div
+            className={`font-14p cursor-pointer bold ${showshoppingcart && 'border-bottom-blue'} width-max-content`}
+            onClick={() => {
+              setshowshoppingcart(true);
+              setshoworder(false);
+              setshoworderhistory(false);
+            }}
+          >
+            Shopping Cart
+          </div>
+          {/*end button to show view shopping cart ------------------------------------------- */}
+
+          {/*start button to show view current orders cart ------------------------------------------- */}
+          <div
+            className={`font-14p cursor-pointer ml-5 bold ${showorder && 'border-bottom-blue'} width-max-content`}
+            onClick={() => {
+              setshowshoppingcart(false);
+              setshoworder(true);
+              setshoworderhistory(false);
+            }}
+          >
+            Current Orders
+          </div>
+          {/*end button to show view current orders cart ------------------------------------------- */}
 
 
-    {/*start button to show view history orders cart ------------------------------------------- */} 
-      <div
-        className={`font-14p cursor-pointer ml-5 bold ${showorderhistory && 'border-bottom-blue'} width-max-content`}
-        onClick={() => {
-          setshowshoppingcart(false);
-          setshoworder(false);
-          setshoworderhistory(true);
-        }}
-      >
-        Past Orders
-      </div> 
-    {/*end button to show view history orders cart ------------------------------------------- */} 
-    </div>
-
+          {/*start button to show view history orders cart ------------------------------------------- */}
+          <div
+            className={`font-14p cursor-pointer ml-5 bold ${showorderhistory && 'border-bottom-blue'} width-max-content`}
+            onClick={() => {
+              setshowshoppingcart(false);
+              setshoworder(false);
+              setshoworderhistory(true);
+            }}
+          >
+            Past Orders
+          </div>
+          {/*end button to show view history orders cart ------------------------------------------- */}
+        </div>
+      </Col>
       {/*Start of shopping cart page-------------------------------------------------------------------*/}
       {showshoppingcart && (
         <>
-        <Row className='offset-lg-2 offset-md-1'>
-          <Col lg={5}>
-          {loginState.productcart.length === 0 ? (
-            "Shopping Cart is Empty"
-          ) : (
-            <ProductCards setshowerrorcard={setshowerrorcard} seterrorcardmessage={seterrorcardmessage} />
-          )}
-          </Col>
-        </Row>    
-          TOTAL: {totalprice} 
-       {/**--------------START if totalprice is 0, then do not allow user to check out */}
+          <Row className='offset-lg-2 offset-md-1'>
+            <Col lg={5}>
+              {loginState.productcart.length === 0 ? (
+                "Shopping Cart is Empty"
+              ) : (
+                <ProductCards setshowerrorcard={setshowerrorcard} seterrorcardmessage={seterrorcardmessage} />
+              )}
+            </Col>
+          </Row>
+          TOTAL: {totalprice}
+          {/**--------------START if totalprice is 0, then do not allow user to check out */}
           {totalprice < 1 ? (
             <Button
               style={{ marginLeft: "10%", marginRight: "10%" }}
@@ -137,8 +138,8 @@ const AfterLogin = ({setshowerrorcard, seterrorcardmessage}) => {
 
       {/*Start of orders page---------------------------------------------------------------*/}
       {loginState.productordering.length === 0 ? (showorder && <h2>You do not have any orders yet</h2>) :
-          (showorder && productorderingjsx)
-        }
+        (showorder && productorderingjsx)
+      }
       {/*End of orders page-------------------------------------------------------------------*/}
 
       {/*Start of orderhistory page*/}
